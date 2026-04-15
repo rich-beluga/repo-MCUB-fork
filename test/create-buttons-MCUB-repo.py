bd79379494fa17384d56642bcffd7f09d530ed88
Шмэлька <alichka240784@gmail.com>
2026-04-15 20:09:04 +1000

    Add CreateButtonsMod for inline button creation

---
+++ test/create-buttons-MCUB-repo.py
@@ -0,0 +1,28 @@
+# scop: kernel min v1.1.6.1
+from core.lib.loader.module_base import ModuleBase, command, callback
+import utils
+from typing import Any
+class CreateButtonsMod(ModuleBase):
+    """class-style modules MCUB"""
+    name = 'create-buttons'
+    version = '1.0.0'
+    description = {"ru": 'создать инлайн кнопки', "en": 'create inline buttons'}
+    @command('create-button', doc={'ru': '<count> создать X количество кнопок', "en": "<count> create X count button(s)"})
+    async def create_buttons(self, message: Any) -> None:
+        try:
+            args = int(utils.get_args_raw(message))
+        except ValueError as err:
+            self.log.debug(f'error debug: {err}')
+            await utils.answer(message, f'please typing <code>{self.kernel.custom_prefix}create_buttons 1</code>', as_html=True)
+            return
+        buttons = []
+        for i in range(int(args)):
+            buttons.append([self.callback_button(f'button {i+1}', self.on_click)])
+        await self.kernel.inline_form(
+            message.chat_id,
+            f'Buttons created: {args}',
+            buttons=buttons
+        )
+    @callback(ttl=50)
+    async def on_click(self, call) -> None:
+        await call.answer('click the buttons', alert=False)
