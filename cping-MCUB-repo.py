9156015ca0ceb21416538abfcae5ebb0089bd448
Шмэлька <alichka240784@gmail.com>
2026-04-17 21:10:20 +1000

    Refactor inline ping module and improve error handling

--- cping-MCUB-repo.py
+++ cping-MCUB-repo.py
@@ -1,73 +1,82 @@
-# requires: aiohttp
-# author: @Hairpin01
-# version: 1.0.3
-# description: пинг в инлайне
-
+# scop: kernel min v1.2.7.2
+import aiohttp
 import time
 import asyncio
-from telethon import events, Button
+from telethon import events
+from typing import Any
 
-def register(kernel):
-    client = kernel.client
+# sdk
+import utils
+from core.lib.loader.module_base import ModuleBase, command, callback
 
 
-    async def ping_api_telegram():
+class PingInlineMod(ModuleBase):
+    name = "cping-MCUB-repo"
+    description = {"ru": "пинг в инлайне", "en": "inline ping"}
+    version = "1.1.0"
+    author = "@Hairpin00"
+    strings = {
+        "ru": {
+            "ping_text": "<b>📶 Пинг до Telegram API:</b> <code>{ping_result}</code> мс",
+            "error": "<b>❌ Ошибка:</b> {error}",
+            "btn_return": "Ещё раз",
+        },
+        "en": {
+            "ping_text": "<b>📶 Ping to Telegram API:</b> <code>{ping_result}</code> ms",
+            "error": "<b>❌ Error:</b> {error}",
+            "btn_return": "One more time",
+        },
+    }
+
+    async def ping_api_telegram(self) -> float | str:
+        """:return: the delay in ms or the error string."""
         try:
-            import aiohttp
             start = time.time()
             timeout = aiohttp.ClientTimeout(total=5)
             async with aiohttp.ClientSession(timeout=timeout) as session:
-                async with session.get('https://api.telegram.org') as resp:
+                async with session.get("https://api.telegram.org") as resp:
                     end = time.time()
-                    return round((end - start) * 1000, 2)  # мс
-        except ImportError:
-            return "Установите aiohttp"
+                    return round((end - start) * 1000, 2)
         except Exception as e:
-            return f"Ошибка: {str(e)}"
+            self.log.error(f"error cping: {e}")
+            return self.strings("error", error=e)
 
-    @kernel.register.command('cping')
-    # cping
-    async def inline_cping(event):
-
+    @command(
+        "cping",
+        doc={"ru": "отправить инлайн форму пинга", "en": "send inline form ping"},
+    )
+    async def cping_cmd(self, message: events.NewMessage.Event) -> None:
         try:
-            ping_result = await ping_api_telegram()
-
+            ping_result = await self.ping_api_telegram()
 
             if isinstance(ping_result, (int, float)):
-                ping_text = f"<b>📶 Пинг до Telegram API:</b> <code>{ping_result}</code> мс"
+                ping_text: str = self.strings("ping_text", ping_result=ping_result)
             else:
-                ping_text = f"<b>❌ Ошибка:</b> {ping_result}"
+                ping_text: str = self.strings("error", error=ping_result)
 
-
-            buttons = [
-                {"text": "Повторить", "type": "callback", "data": "cping"}
+            buttons: dict[dict[Any]] = [
+                [self.Button.inline(self.strings("btn_return"), self.on_click)]
             ]
 
-            success = await kernel.inline_form(
-                event.chat_id,
-                title=ping_text,
-                buttons=buttons
+            success = await self.kernel.inline_form(
+                message.chat_id, title=ping_text, buttons=buttons
             )
             if success:
-                await event.delete()
-
+                await message.delete()
 
         except Exception as e:
-            await kernel.handle_error(e, source="inline_cping", event=event)
+            await self.kernel.handle_error(e, source="inline_cping", event=message)
 
-    async def callback_cping(event):
-        ping_result = await ping_api_telegram()
-        buttons = [
-            [
-                Button.inline("Повторить", b"cping")
-            ]
-        ]
+    @callback(ttl=100)
+    async def on_click(self, call: events.CallbackQuery.Event) -> None:
+        """callback hanler cping"""
+        ping_result = await self.ping_api_telegram()
+        buttons = [[self.Button.inline(self.strings("btn_return"), self.on_click)]]
 
         if isinstance(ping_result, (int, float)):
-            ping_text = f"<b>📶 Пинг до Telegram API:</b> <code>{ping_result}</code> мс"
+            ping_text: str = self.strings("ping_text", ping_result=ping_result)
         else:
-            ping_text = f"<b>❌ Ошибка:</b> {ping_result}"
+            ping_text: str = self.strings("error", error=ping_result)
+            await call.anwser(self.strings("error", error=ping_text[:20]))
 
-        await event.edit(ping_text, buttons=buttons, parse_mode='html')
-
-    kernel.register_callback_handler("cping", callback_cping)
+        await utils.answer(call, ping_text, buttons=buttons, as_html=True)
